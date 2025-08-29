6fab6b267c638ce57e549b1844c759f77d686de1
arc <zaveyborodaid@gmail.com>
2025-08-29 18:23:18 +0500

    Initial commit

---
+++ gen-all-dissections/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ayaxdd/math/types"
+)
+
+const (
+	msg = "%d: %v\n"
+)
+
+func main() {
+	test := []string{"a", "b", "c", "e", "f", "g", "h"}
+	all := GenAllDissections(test)
+	for i, subsets := range all {
+		fmt.Printf(msg, i+1, subsets)
+	}
+}
+
+func GenAllDissections(aSet []string) []*types.NSet {
+	que := types.CreateBuffer()
+	ssPut := types.CreateNSet()
+	que.Put(ssPut)
+
+	for _, item := range aSet {
+		for range que.Size() {
+			ssGet := que.Get().(*types.NSet)
+			localHandle(item, ssGet, que)
+		}
+	}
+
+	res := make([]*types.NSet, 0, que.Size())
+	for que.Size() > 0 {
+		res = append(res, que.Get().(*types.NSet))
+	}
+
+	return res
+}
+
+func localHandle(item string, ssGet *types.NSet, que *types.Buffer) {
+	// add item to all existing substets
+	for i := range ssGet.Size() {
+		ssPut := ssGet.Copy()
+		s := ssPut.GetItem(i)
+		ssPut.Delete(i)
+		s.Insert(item)
+		ssPut.Insert(s)
+		que.Put(ssPut)
+	}
+
+	// create and add new {item} subset
+	ssPut := ssGet.Copy()
+	s := types.CreateSet()
+	s.Insert(item)
+	ssPut.Insert(s)
+	que.Put(ssPut)
+}
